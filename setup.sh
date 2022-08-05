3c680e2fc993612109aeb229c72144fdf44a50ad
kai <84999468+synthun@users.noreply.github.com>
2022-08-04 20:41:49 -0400

    added untested support for arch linux

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ fi
 
 echo && echo "using $root" && echo
 
-echo "what distro are you on (supported options: void)"
+echo "what distro are you on (supported options: void, arch)"
 read -p "enter here: "
 
 void_install() {
@@ -71,7 +71,57 @@ void_install() {
 	esac
 }
 
+arch_install() {
+	echo && echo "updating before installation" && echo && sleep 1s
+	$root pacman -Syu
+
+	echo && echo "installing required packages" && echo & sleep 1s
+	$root pacman -S xorg-server git xorg-xinit alacritty dunst sxhkd base-devel
+
+        echo && echo "starting setup" && echo & sleep 1s
+        mkdir -p $HOME/.config
+        mkdir -p $HOME/.local/share/fonts/
+
+        rm -rfv $HOME/.config/dmenu
+        rm -rfv $HOME/.config/dwm
+        rm -rfv $HOME/.config/dotfiles
+        rm -rfv $HOME/.config/fonts
+
+        cd $HOME/.config
+
+        git clone https://github.com/synthun/dwm
+        git clone https://github.com/synthun/dmenu
+        git clone https://github.com/synthun/dotfiles
+        git clone https://github.com/synthun/fonts
+
+        cd
+
+        cd $HOME/.config/dwm && $root make clean install
+        cd $HOME/.config/dmenu && $root make clean install
+
+        cp -r $HOME/.config/dotfiles/sxhkd $HOME/.config
+        cp -r $HOME/.config/dotfiles/dunst $HOME/.config
+        cp -r $HOME/.config/dotfiles/alacritty $HOME/.config
+        cp $HOME/.config/dotfiles/xinitrc $HOME/.xinitrc
+
+        cp $HOME/.config/fonts/jetbrains-mono/* $HOME/.local/share/fonts
+        cp $HOME/.config/fonts/san-francisco/* $HOME/.local/share/fonts
+
+        echo && echo "cleaning up" && echo & sleep 1s
+        rm -rfv $HOME/.config/dotfiles
+        rm -rfv $HOME/.config/fonts
+
+        read -p "reboot? (optional) (y/n): "
+
+        case $REPLY in
+                y) $root reboot;;
+                n) echo && echo "installation complete, config files for dwm and dmenu located in $HOME/.config";;
+                *) echo && echo "option $REPLY invalid, not rebooting." && echo "installation complete, config files for dwm and dmenu located in $HOME/.config"
+        esac
+}
+
 case $REPLY in
 	void) void_install;;
+	arch) arch_install;;
 	*) echo "$REPLY does not exist or is not supported"
 esac
