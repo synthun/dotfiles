9cc6a8d8e003300e99f4e452f4f7aa66540c4e84
synthun <kai6135@proton.me>
2022-09-18 10:48:12 -0400

    neovim (again)

---
+++ nvim/init.vim
@@ -0,0 +1,4 @@
+colorscheme gruvbox
+autocmd VimEnter * hi Normal ctermbg=none
+set number
+highlight Comment cterm=italic gui=italic
--- vis/visrc.lua
+++
@@ -1,8 +0,0 @@
--- load standard vis module, providing parts of the Lua API
-require('vis')
-
-vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	-- Your per window configuration options e.g.
-	-- vis:command('set number')
-	vis:command('set theme base16-gruvbox-dark-hard')
-end)
